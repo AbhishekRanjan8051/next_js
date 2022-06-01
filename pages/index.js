41e19d5e6d9baddadb1d45a162ad19e0da6f4628
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2022-06-01 10:44:09 +0530

    try to get bolg through api

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
       <main className={styles.main}>
         <h1>
           <a>Welcome to Abhishek's Blog post 🙏</a>
@@ -51,19 +50,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
